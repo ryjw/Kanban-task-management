a6e3338e96d00b0441c06156d3a18d5ec81f2234
jazz <jazxbx@gmail.com>
2023-12-18 09:31:33 -0800

    sass variables added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "../App.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Kanban Task Manager",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={plus_jakarta_sans.className}>{children}</body>
     </html>
   );
 }
