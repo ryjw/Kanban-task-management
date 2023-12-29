c8ebc8fe127f014545e1a2e4e08b807a1c288f71
Ryan Wilson <ryanjoelwilson@gmail.com>
2023-12-29 11:01:30 +0000

    initial textfield styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,8 @@
 import Button from "@/components/Button.jsx";
 import Card from "../components/Card.jsx";
 import { Test } from "@/components/Test.jsx";
+import TextField from "@/components/TextField.jsx";
 
 export default function Home() {
-  return (
-    <>
-      <Card />
-      <Button content={"+ Create New Board"} variant={"btn-board"} />
-      <Button
-        content={"+ Create New Board (Modal Version)"}
-        variant="default"
-      />
-      <Button content={"Delete"} variant="danger" />
-      <Button content={"Cancel"} variant="secondary" />
-
-      <Test />
-    </>
-  );
+  return <TextField />;
 }
