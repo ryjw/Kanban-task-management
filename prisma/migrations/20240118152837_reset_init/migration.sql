6f0a11d55af9d035600fc2a2cc05bc76864b2f5e
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-01-18 16:02:15 +0000

    started API doc

--- prisma/migrations/20240118152837_reset_init/migration.sql
+++ prisma/migrations/20240118152837_reset_init/migration.sql
@@ -21,7 +21,6 @@ CREATE TABLE "Column" (
     "id" STRING NOT NULL,
     "name" STRING NOT NULL,
     "boardId" STRING NOT NULL,
-    "userId" STRING NOT NULL,
 
     CONSTRAINT "Column_pkey" PRIMARY KEY ("id")
 );
@@ -30,9 +29,8 @@ CREATE TABLE "Column" (
 CREATE TABLE "Task" (
     "id" STRING NOT NULL,
     "name" STRING NOT NULL,
-    "description" STRING,
+    "description" STRING DEFAULT '',
     "columnId" STRING NOT NULL,
-    "userId" STRING NOT NULL,
 
     CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
 );
@@ -43,10 +41,21 @@ CREATE TABLE "Subtask" (
     "name" STRING NOT NULL,
     "taskId" STRING NOT NULL,
     "isFulfilled" BOOL NOT NULL DEFAULT false,
-    "userId" STRING NOT NULL,
 
     CONSTRAINT "Subtask_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Board" ADD CONSTRAINT "Board_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Column" ADD CONSTRAINT "Column_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_columnId_fkey" FOREIGN KEY ("columnId") REFERENCES "Column"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Subtask" ADD CONSTRAINT "Subtask_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "Task"("id") ON DELETE CASCADE ON UPDATE CASCADE;
