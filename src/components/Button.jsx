7ca580f013fb25c6935bed90d740d4ebf5bb5387
Krizza Bullecer <krizzabullecer@gmail.com>
2024-01-21 14:47:16 +0900

    - update button styling - add default values for props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,16 @@
-import styles from "../partials/_button.module.scss";
+import styles from '@/partials/_button.module.scss';
 
-export default function Button({ content, variant, fun }) {
+export default function Button({
+  content,
+  variant = 'default',
+  size = 'sm',
+  onClick,
+}) {
   return (
-    <button onClick={fun} className={styles[variant]}>
+    // for reference if you want to use classnames library:
+    // classnames(styles[variant], styles[size])
+    // `${styles[variant]} ${styles[size]}`
+    <button onClick={onClick} className={`${styles[variant]} ${styles[size]}`}>
       {content}
     </button>
   );
