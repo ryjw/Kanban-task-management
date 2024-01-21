1f24e2d1c8207567fd5004561a26816f2d50191e
Krizza Bullecer <krizzabullecer@gmail.com>
2024-01-22 00:27:56 +0900

    change button content prop to children

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,17 @@
 import styles from '@/partials/_button.module.scss';
 
 export default function Button({
-  content,
   variant = 'default',
   size = 'sm',
   onClick,
+  children,
 }) {
   return (
     // for reference if you want to use classnames library:
     // classnames(styles[variant], styles[size])
     // `${styles[variant]} ${styles[size]}`
     <button onClick={onClick} className={`${styles[variant]} ${styles[size]}`}>
-      {content}
+      {children}
     </button>
   );
 }
