aff20425b4cb2688456a758a2358fda361f7e5e5
Krizza Bullecer <krizzabullecer@gmail.com>
2024-01-21 14:48:51 +0900

    - fix sidebar - move sidebar in layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,9 @@
-import styles from "../partials/_sidebar.module.scss";
-import logo from "../../public/assets/logo.png";
-import shape from "../../public/assets/shape.png";
-import OpenEye from "../../public/assets/OpenEye.png";
-import HideSidebar from "../../public/assets/HideSidebar.png";
-import Image from "next/image";
+import styles from '@/partials/_sidebar.module.scss';
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <div>
-        <h1>
-          <Image src={logo} /> kanban
-        </h1>
-        <p> ALL BOARDS (0)</p>
-        <button>
-          <Image src={shape} className={styles.sidebarIcon} />+ Create New Board
-        </button>
-      </div>
-      <div className={styles.sidebarToggle}>
-        <div className={styles.HideSidebar}>
-          <Image src={HideSidebar} />
-        </div>
-        <div>
-          <Image className={styles.OpenEye} src={OpenEye} />
-        </div>
-      </div>
+      <div>FIXME: sidebar</div>
     </div>
   );
 }
