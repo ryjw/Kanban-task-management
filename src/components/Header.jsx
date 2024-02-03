d97912120be50d4f17d2cf4a032ee01be6af32bc
jazz <jazxbx@gmail.com>
2024-02-02 22:59:42 -0800

    added media queries to hide/display elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,37 @@ import Button from "./Button";
 import DropdownMenu from "./DropdownMenu";
 import logoMobile from "@/assets/logo-mobile.svg";
 import iconChevronDown from "@/assets/icon-chevron-down.svg";
+import logoDesktop from "@/assets/logo-dark.svg";
 import Image from "next/image";
 
 function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.boardName}>
-        <Image src={logoMobile} alt="logo mobile" />
+        <Image
+          src={logoDesktop}
+          alt="logo desktop"
+          className={styles.logoDesktop}
+        />
+        <Image
+          src={logoMobile}
+          alt="logo mobile"
+          className={styles.logoMobile}
+        />
         <p>Platform Launch</p>
         <Image src={iconChevronDown} alt="toggle" />
       </div>
       <div className={styles.btnMenu}>
-        <div>
+        <div className={styles.btnMobile}>
           <Button>+</Button>
         </div>
-        <DropdownMenu />
+        <div className={styles.btnDesktop}>
+          <Button>+ Add New Task</Button>
+        </div>
+        <DropdownMenu
+          options={["Edit Board", "Delete Board"]}
+          variant="header"
+        />
       </div>
     </header>
   );
