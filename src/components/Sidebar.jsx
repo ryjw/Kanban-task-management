7c7390b8a95eafc00f49dcacef918f4a4d65cb41
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-02-15 12:32:50 +0000

    basic list styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState } from "react";
 import styles from "./Sidebar.module.scss";
+import iconBoard from "@/assets/icon-board-split.svg";
+import Image from "next/image";
 
 export default function Sidebar() {
   const [boards, setboards] = useState(["boardOne", "boardTwo"]);
@@ -8,7 +10,23 @@ export default function Sidebar() {
   return (
     <div className={styles.outerSidebar}>
       <div className={styles.innerSidebar}>
-        <h2 className={styles.subheading}>ALL BOARDS {`(${boards.length})`}</h2>
+        <ul>
+          <h2 className={styles.subheading}>
+            ALL BOARDS {`(${boards.length})`}
+          </h2>
+          {boards.map((board) => {
+            return (
+              <li className={styles.boardList}>
+                <Image src={iconBoard} alt="board icon" />
+                <div>{board}</div>
+              </li>
+            );
+          })}
+          <li className={styles.boardList}>
+            <Image src={iconBoard} alt="board icon" />
+            <div className={styles.newBoard}>+ Create New Board</div>
+          </li>
+        </ul>
       </div>
     </div>
   );
