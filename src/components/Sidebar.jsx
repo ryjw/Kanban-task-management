a2ec9c0d7e3a320cdab2f8e99fe78d474e8cf0e9
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-02-15 11:13:52 +0000

    separated inner and outer sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,8 +6,10 @@ export default function Sidebar() {
   const [boards, setboards] = useState(["boardOne", "boardTwo"]);
 
   return (
-    <div className={styles.sidebar}>
-      <h2 className={styles.subheading}>ALL BOARDS {`(${boards.length})`}</h2>
+    <div className={styles.outerSidebar}>
+      <div className={styles.innerSidebar}>
+        <h2 className={styles.subheading}>ALL BOARDS {`(${boards.length})`}</h2>
+      </div>
     </div>
   );
 }
