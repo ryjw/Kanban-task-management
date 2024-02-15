78a1f6c369b3a0f262679cb10709cec6574c88d9
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-02-15 11:24:15 +0000

    made header background white

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,12 @@
-import { Plus_Jakarta_Sans } from 'next/font/google';
-import Sidebar from '@/components/Sidebar';
-import './globals.scss';
+import { Plus_Jakarta_Sans } from "next/font/google";
+import Sidebar from "@/components/Sidebar";
+import "./globals.scss";
 
-const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ['latin'] });
+const plus_jakarta_sans = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Kanban Task Manager',
-  description: 'Your first stop for organisation!',
+  title: "Kanban Task Manager",
+  description: "Your first stop for organisation!",
 };
 
 export default function RootLayout({ children }) {
