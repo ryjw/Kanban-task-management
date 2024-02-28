0f52d99059c2152d94b9a222f2cec8882493ec67
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-02-28 10:22:05 +0000

    added fetch request to server, redirecting unauthenticated users to /login

---
+++ src/app/login/page.js
@@ -0,0 +1,43 @@
+"use client";
+import { useState } from "react";
+import Button from "@/components/Button";
+import Modal from "@/components/Modal";
+import LoginForm from "@/forms/LoginForm";
+import RegisterForm from "@/forms/RegisterForm";
+
+export default function Login() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
+  //toggle between login and register
+  const toggleForm = () => {
+    setShowLogin((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button size="lg" onClick={() => setModalOpen(true)}>
+          Please log in to access Kanban Task Manager
+        </Button>
+      </div>
+      {modalOpen && (
+        <Modal open={modalOpen} setOpen={setModalOpen}>
+          {showLogin ? (
+            <LoginForm onToggle={toggleForm} />
+          ) : (
+            <RegisterForm onToggle={toggleForm} />
+          )}
+        </Modal>
+      )}
+    </>
+  );
+}
--- src/modules/EmptyState.jsx
+++ src/modules/EmptyState.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import Button from "@/components/Button";
 import Modal from "@/components/Modal";
 import TextField from "@/components/TextField";
-
 import styles from "./EmptyState.module.scss";
+import LoginForm from "@/forms/LoginForm";
 
 export default function EmptyState() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -17,13 +17,7 @@ export default function EmptyState() {
       {/* Example use of modal, you will need to store the modal visibility state,
       in this example, the state is stored in `modalOpen` */}
       <Modal open={modalOpen} setOpen={setModalOpen}>
-        {/* <form style={{ display: 'flex', flexDirection: 'column' }}>
-          <TextField variant="default" placeholder="Title" />
-          <TextField variant="default" />
-          <TextField variant="default" />
-          <TextField variant="default" />
-          <Button onClick={() => console.log('test')}>Submit</Button>
-        </form> */}
+        {} // new board modal goes here
       </Modal>
     </div>
   );
