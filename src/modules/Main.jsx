c4b1d9ab04b9b5dad26e5c080706e82e1a5d0066
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-02-29 14:14:35 +0000

    better css

--- src/modules/Main.jsx
+++ src/modules/Main.jsx
@@ -3,15 +3,18 @@ import styles from "@/modules/Main.module.scss";
 export default function Main({ currentBoard }) {
   return (
     <div className={styles.outerDiv}>
-      {Object.keys(currentBoard).length >= 1 ? (
+      {Object.keys(currentBoard).length > 0 ? (
         <div className={styles.columnContainer}>
-          {currentBoard.columns.map((column) => {
-            return (
-              <div className={styles.column} key={column.id}>
-                {column.name}
-              </div>
-            );
-          })}
+          {currentBoard.columns
+            .slice()
+            .reverse()
+            .map((column) => {
+              return (
+                <div className={styles.column} key={column.id}>
+                  {`${column.name} (${column.tasks.length})`}
+                </div>
+              );
+            })}
           {console.log(Object.keys(currentBoard).length, currentBoard.columns)}
           <div className={styles.bigButton}>
             <button className={styles.invisibleButton}>+ New Column</button>
